2a59300b3c6155e6156e42e63396441dfa0191f9
Sekamatte Soul Solomon <99466179+SoulSolomon21@users.noreply.github.com>
2024-05-20 13:05:56 +0300

    create hero section

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,12 @@ import HeroSection from "@/components/global/hero-section";
 
 export default function Home() {
 
-  return (
-    <main className="">
-      <HeroSection />
-    </main>
-  );
+    return (
+        <main
+            className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
+            <div className="max-w-7xl w-full">
+                <HeroSection/>
+            </div>
+        </main>
+    );
 }
--- components/global/hero-section.tsx
+++ components/global/hero-section.tsx
@@ -1,39 +1,59 @@
 "use client"
 
 import React from 'react';
-import {useScroll, useTransform} from "framer-motion";
-import {GoogleGeminiEffect} from "@/components/ui/google-gemini-effect";
+import {Spotlight} from "@/components/ui/spotlight";
+import {TextGenerateEffect} from "@/components/ui/text-generate-effect";
+import Link from "next/link";
+import MagicButton from "@/components/ui/MagicButton";
+import {Navigation} from "lucide-react";
 
 const HeroSection = () => {
-    const ref = React.useRef(null);
-    const { scrollYProgress } = useScroll({
-        target: ref,
-        offset: ["start start", "end start"],
-    });
-
-    const pathLengthFirst = useTransform(scrollYProgress, [0, 0.8], [0.2, 1.2]);
-    const pathLengthSecond = useTransform(scrollYProgress, [0, 0.8], [0.15, 1.2]);
-    const pathLengthThird = useTransform(scrollYProgress, [0, 0.8], [0.1, 1.2]);
-    const pathLengthFourth = useTransform(scrollYProgress, [0, 0.8], [0.05, 1.2]);
-    const pathLengthFifth = useTransform(scrollYProgress, [0, 0.8], [0, 1.2]);
-
     return (
-        <div
-            className="h-[400vh] bg-black w-full dark:border dark:border-white/[0.1] rounded-md relative pt-40 overflow-clip"
-            ref={ref}
-        >
-            <GoogleGeminiEffect
-            pathLengths={[
-                pathLengthFirst,
-                pathLengthSecond,
-                pathLengthThird,
-                pathLengthFourth,
-                pathLengthFifth,
-            ]}
-            title={"Software Engineering Chapter"}
-            description={"From Beginner to Builder: Join the Software Engineering Chapter!"}
-            className="px-2 top-10"
-        />
+        <div className="pb-20 pt-32">
+            <div>
+                <Spotlight
+                    className="-top-40 -left-10 md:-left-32 md:-top-20 h-screen"
+                    fill="white"
+                />
+                <Spotlight
+                    className="h-[80vh] w-[50vw] top-10 left-full"
+                    fill="purple"
+                />
+                <Spotlight className="left-80 top-28 h-[80vh] w-[50vw]" fill="blue"/>
+            </div>
+
+            <div
+                className="h-screen w-full bg-black-100 bg-grid-white/[0.05] absolute top-0 left-0 flex items-center justify-center"
+            >
+                <div
+                    className="absolute pointer-events-none inset-0 flex items-center justify-center bg-black-100 [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"
+                />
+            </div>
+
+            <div className="flex justify-center relative my-20 z-10">
+                <div className="max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center">
+                    <p className="uppercase tracking-widest text-xs text-center text-blue-100 max-w-80">
+                        From Beginner to Builder: Join the Software Engineering Chapter! 👨🏽‍💻
+                    </p>
+
+                    <TextGenerateEffect
+                        className="text-center text-[40px] md:text-5xl lg:text-6xl"
+                        words={"UCU Software Engineering Chapter"}
+                    />
+
+                    <p className="text-center text-white-100 md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl">
+                        Hi there, excited to have you check us out
+                    </p>
+
+                    <Link href={"#"}>
+                        <MagicButton
+                            title={"Join Now"}
+                            icon={<Navigation/>}
+                            position={"right"}
+                        />
+                    </Link>
+                </div>
+            </div>
         </div>
     );
 };
